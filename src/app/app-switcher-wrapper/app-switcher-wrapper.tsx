03ee08fb18c2a7bc3d65f10c9de517729b445206
John McNally <jmcnally@digital.ai>
2023-09-05 06:54:09 -0700

    Use released version of dot-components. Some formatting changes

--- src/app/app-switcher-wrapper/app-switcher-wrapper.tsx
+++ src/app/app-switcher-wrapper/app-switcher-wrapper.tsx
@@ -1,7 +1,25 @@
-import {AfterViewInit, Component, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, Output, SimpleChanges, ViewChild} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  // EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  // Output,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {DotAppSwitcher, DotCoreApiProvider, DotIconButton, DotThemeProvider, DotTypography, useDotCoreApiContext} from '@digital-ai/dot-components';
+import {
+  DotAppSwitcher,
+  DotCoreApiProvider,
+  DotIconButton,
+  DotThemeProvider,
+  DotTypography,
+  useDotCoreApiContext
+} from '@digital-ai/dot-components';
 
 const containerElementName = 'dotAppSwitcherContainer';
 
@@ -10,7 +28,9 @@ interface AppSwitcherButtonProps {
   tooltip?: string;
 }
 
-function AppSwitcherButton({ iconId = 'apps', tooltip = "Toggle app switcher" }: AppSwitcherButtonProps) {
+function AppSwitcherButton(
+  { iconId = 'apps', tooltip = "Toggle app switcher" }: AppSwitcherButtonProps
+) {
   const { setIsAppSwitcherOpen } = useDotCoreApiContext();
 
   const toggleAppSwitcher = () => {
@@ -34,60 +54,60 @@ interface ActiveAppInfo {
 }
 
 @Component({
-    selector: 'dot-app-switcher',
-    template: `<span #${containerElementName}></span>`
+  selector: 'dot-app-switcher',
+  template: `<span #${containerElementName}></span>`
 })
 export class DotAppSwitcherWrapperComponent implements OnChanges, OnDestroy, AfterViewInit {
-    @Input() public buttonIconId = 'apps';
-    @Input() public buttonTooltip = "Toggle app switcher";
-    @Input() public platformApi: string = 'foo';
-    @Input() public includePlatformConsole = true;
-    @Input() public yOffset = 56;
-    @Input() public zIndex = 990;
-    @Input() public activeApp: ActiveAppInfo = { name: 'Undefined', product: 'Agility'};
-    @Input() public maxRecentInstances = 5;
-    @Input() public noAppTypeLabel = 'Other';
-    @Input() public searchInstancesThreshold = 5;
-    @Output() public onClose = new EventEmitter<void>();
-    @ViewChild(containerElementName, {static: true}) containerRef!: ElementRef;
+  @Input() public buttonIconId = 'apps';
+  @Input() public buttonTooltip = "Toggle app switcher";
+  @Input() public platformApi: string = 'https://demo-mock-api';
+  @Input() public token: string = 'jwt; ignored for demo data';
+  @Input() public includePlatformConsole = true;
+  @Input() public yOffset = 56;
+  @Input() public zIndex = 990;
+  @Input() public activeApp: ActiveAppInfo = { name: 'Undefined', product: 'Agility'};
+  @Input() public maxRecentInstances = 5;
+  @Input() public noAppTypeLabel = 'Other';
+  @Input() public searchInstancesThreshold = 5;
+  // @Output() public onClose = new EventEmitter<void>();
+  @ViewChild(containerElementName, {static: true}) containerRef!: ElementRef;
 
-    ngOnChanges(changes: SimpleChanges): void {
-        this.render();
-    }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.render();
+  }
 
-    ngAfterViewInit() {
-        this.render();
-    }
+  ngAfterViewInit() {
+    this.render();
+  }
 
-    ngOnDestroy() {
-        ReactDOM.unmountComponentAtNode(this.containerRef.nativeElement);
-    }
+  ngOnDestroy() {
+    ReactDOM.unmountComponentAtNode(this.containerRef.nativeElement);
+  }
 
-    handleOnClose() {
-        if (this.onClose) {
-            this.onClose.emit();
-        }
-    }
+  handleOnClose() {
+    // if (this.onClose) {
+    //   this.onClose.emit();
+    // }
+  }
 
-    private render() {
-      ReactDOM.render(
-        <DotThemeProvider>
-          <DotCoreApiProvider apiUrl={this.platformApi}>
-            <AppSwitcherButton iconId={this.buttonIconId} tooltip={this.buttonTooltip} />
-            <DotAppSwitcher
-              includePlatformConsole={this.includePlatformConsole}
-              yOffset={this.yOffset}
-              zIndex={this.zIndex}
-              activeApp={this.activeApp}
-              maxRecentInstances={this.maxRecentInstances}
-              noAppTypeLabel={this.noAppTypeLabel}
-              searchInstancesThreshold={this.searchInstancesThreshold}
-              onClose={this.handleOnClose}
-            />
-          </DotCoreApiProvider>
-        </DotThemeProvider>
-        ,
-        this.containerRef.nativeElement
-      );
-    }
+  private render() {
+    ReactDOM.render(
+      <DotThemeProvider>
+        <DotCoreApiProvider apiUrl={this.platformApi} token={this.token}>
+          <AppSwitcherButton iconId={this.buttonIconId} tooltip={this.buttonTooltip} />
+          <DotAppSwitcher
+            includePlatformConsole={this.includePlatformConsole}
+            yOffset={this.yOffset}
+            zIndex={this.zIndex}
+            activeApp={this.activeApp}
+            maxRecentInstances={this.maxRecentInstances}
+            noAppTypeLabel={this.noAppTypeLabel}
+            searchInstancesThreshold={this.searchInstancesThreshold}
+            onClose={this.handleOnClose}
+          />
+        </DotCoreApiProvider>
+      </DotThemeProvider>,
+      this.containerRef.nativeElement
+    );
+  }
 }
